fb44cc75bfdf3f2b3df2dd26df07bd8f5e1a9e47
Heathcliff <2826190541@qq.com>
2023-12-25 22:48:23 +0800

    local db

--- src/lyrics/query.rs
+++ src/lyrics/query.rs
@@ -5,6 +5,7 @@ use log::info;
 use log::warn;
 use lrc::Lyrics;
 use reqwest::blocking::Client;
+use rusqlite::{params, Connection};
 
 pub struct Query {
     client: Client,
@@ -22,9 +23,6 @@ impl Query {
             last_query: String::from(""),
         }
     }
-
-    // TODO: async
-    // TODO: refactor
     pub fn get_lyrics(
         &mut self,
         name: &str,
@@ -39,51 +37,111 @@ impl Query {
                 return Ok(true);
             }
             info!("{}", &self.last_query);
-            *lyrics = None;
-            let response = self
-                .client
-                .get("https://lyrics-api.lujjjh.com/")
-                .query(&[("name", name), ("artist", artist)])
-                .send()
-                .map_err(|e| {
-                    error!("Network error: {:?}", e);
-                    e
-                })?
-                .error_for_status()
-                .map_err(|e| {
-                    warn!("Bad status: {:?}", e);
+
+            // 连接到本地数据库
+            let conn = Connection::open("./songs.db")?;
+            let mut stmt = conn.prepare("SELECT lyrics FROM songs WHERE name=? AND artist=?")?;
+
+            let rows = stmt.query_map(params![name, artist], |row| row.get(0))?;
+
+            // 如果找到歌词
+            if let Some(row) = rows.into_iter().next() {
+                let lyrics_text: String = row?;
+                let downloaded_lyrics = Lyrics::from_str(&lyrics_text).map_err(|e| {
+                    error!("Failed to parse lyrics: {:?}", e);
                     e
                 })?;
-            let body = response.text().map_err(|e| {
-                error!("Failed to read response body: {:?}", e);
-                e
-            })?;
-            let downloaded_lyrics = Lyrics::from_str(body).map_err(|e| {
-                error!("Failed to parse lyrics: {:?}", e);
-                e
-            })?;
-            info!("OK");
-            let mut new_lyrics = Lyrics::new();
-            let timed_lines = downloaded_lyrics.get_timed_lines();
-            for (i, (time_tag, line)) in timed_lines.iter().enumerate() {
-                let line = html_escape::decode_html_entities(&line);
-                let text = line.trim();
-                // Skip empty lines that last no longer than 3s.
-                if text.is_empty() {
-                    if i < timed_lines.len() - 1 {
-                        let duration =
-                            timed_lines[i + 1].0.get_timestamp() - time_tag.get_timestamp();
-                        if duration <= 3000 {
-                            continue;
+                info!("OK");
+                let mut new_lyrics = Lyrics::new();
+                let timed_lines = downloaded_lyrics.get_timed_lines();
+                for (i, (time_tag, line)) in timed_lines.iter().enumerate() {
+                    let line = html_escape::decode_html_entities(&line);
+                    let text = line.trim();
+                    if text.is_empty() {
+                        if i < timed_lines.len() - 1 {
+                            let duration =
+                                timed_lines[i + 1].0.get_timestamp() - time_tag.get_timestamp();
+                            if duration <= 3000 {
+                                continue;
+                            }
                         }
                     }
+                    new_lyrics.add_timed_line(time_tag.clone(), line)?;
                 }
-                new_lyrics.add_timed_line(time_tag.clone(), line)?;
+                *lyrics = Some(new_lyrics);
+            } else {
+                *lyrics = None;
             }
-            *lyrics = Some(new_lyrics);
+
             Ok(true)
         } else {
             Ok(false)
         }
     }
 }
+// TODO: async
+// TODO: refactor
+//     pub fn get_lyrics(
+//         &mut self,
+//         name: &str,
+//         artist: &str,
+//         lyrics: &mut Option<Lyrics>,
+//     ) -> Result<bool, Box<dyn std::error::Error>> {
+//         let query = format!("{} {}", name, artist);
+//         if self.last_query != query {
+//             self.last_query = query;
+//             if name.is_empty() || artist.is_empty() {
+//                 *lyrics = None;
+//                 return Ok(true);
+//             }
+//             info!("{}", &self.last_query);
+//             *lyrics = None;
+//             let response = self
+//                 .client
+//                 .get("https://lyrics-api.lujjjh.com/")
+//                 .query(&[("name", name), ("artist", artist)])
+//                 .send()
+//                 .map_err(|e| {
+//                     error!("Network error: {:?}", e);
+//                     e
+//                 })?
+//                 .error_for_status()
+//                 .map_err(|e| {
+//                     warn!("Bad status: {:?}", e);
+//                     e
+//                 })?;
+//             // true url
+//             info!("{}", response.url());
+//             let body = response.text().map_err(|e| {
+//                 error!("Failed to read response body: {:?}", e);
+//                 e
+//             })?;
+//             let downloaded_lyrics = Lyrics::from_str(body).map_err(|e| {
+//                 error!("Failed to parse lyrics: {:?}", e);
+//                 e
+//             })?;
+//             info!("OK");
+//             let mut new_lyrics = Lyrics::new();
+//             let timed_lines = downloaded_lyrics.get_timed_lines();
+//             for (i, (time_tag, line)) in timed_lines.iter().enumerate() {
+//                 let line = html_escape::decode_html_entities(&line);
+//                 let text = line.trim();
+//                 // Skip empty lines that last no longer than 3s.
+//                 if text.is_empty() {
+//                     if i < timed_lines.len() - 1 {
+//                         let duration =
+//                             timed_lines[i + 1].0.get_timestamp() - time_tag.get_timestamp();
+//                         if duration <= 3000 {
+//                             continue;
+//                         }
+//                     }
+//                 }
+//                 new_lyrics.add_timed_line(time_tag.clone(), line)?;
+//             }
+//             *lyrics = Some(new_lyrics);
+//             Ok(true)
+//         } else {
+//             Ok(false)
+//         }
+//     }
+// }
